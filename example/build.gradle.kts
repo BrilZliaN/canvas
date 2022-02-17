b7e6b9cda719705fd33998305702df96e00a324a
Ilya Zakharov <brilzlian@ya.ru>
2022-02-17 14:50:14 +0300

    Update Github Actions

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
     implementation(project(":"))
-    implementation("com.github.Minestom:Minestom:1b55bd0137")
+    implementation("com.github.Minestom:Minestom:be100fa5b8")
 }
